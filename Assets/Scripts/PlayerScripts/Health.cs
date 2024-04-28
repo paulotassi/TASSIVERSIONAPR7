d87e2879c0c0b9b675719f896f0f1198d0536d14
paulotassi <paulo.r.tassi@gmail.com>
2024-04-28 13:48:34 -0400

    Functioning Healthbar

--- Assets/Scripts/PlayerScripts/Health.cs
+++ Assets/Scripts/PlayerScripts/Health.cs
@@ -9,6 +9,7 @@ public class Health : MonoBehaviour
     // Start is called before the first frame update
     public int maxHealth = 5;
     public int currentHealth;
+
     void Start()
     {
         currentHealth = maxHealth;
@@ -16,6 +17,7 @@ public class Health : MonoBehaviour
     public void takeDamage(int amount)
     {
         currentHealth -= amount;
+
         if (currentHealth <= 0) {
             Debug.Log("DEAD Thing");
             Destroy(gameObject);
